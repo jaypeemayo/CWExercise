09583bb4d49a05b3b0f804c6d2f2e35e9e5a6d9f
Jaypee Mayo <jmayo@starrez.com>
2021-07-29 07:18:11 +1000

    rename to cw

---
+++ CWExerciseUnitTest/AuthenticationService.unit.test.cs
@@ -0,0 +1,44 @@
+
+using CWExerciseWebApi.BLL;
+using CWExerciseWebApi.BLL.Services;
+using CWExerciseWebApi.Common.Authentication;
+using CWExerciseWebApi.Common.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace CWExerciseUnitTest
+{
+    [TestClass]
+    public class AuthenticationServiceTest
+    {
+        private Mock<IConfiguration> mockConfigurationService;
+        private AuthenticationService service;
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            mockConfigurationService = new Mock<IConfiguration>();
+            mockConfigurationService.SetupGet(x => x["Jwt:Key"]).Returns("veryVerySecretKey");
+            mockConfigurationService.SetupGet(x => x["Jwt:Issuer"]).Returns("http://localhost:2550");
+
+            service = new AuthenticationService(mockConfigurationService.Object);
+        }
+
+        [TestMethod]
+        public void CreateToken_ValidUserNameAndPassword_ReturnToken()
+        {              
+            var token = service.CreateToken(new LoginModel() { Username= "admin", Password= "TestPass!123" });
+            Assert.IsNotNull(token);
+        }
+
+        [TestMethod]
+        public void CreateToken_InvalidUserNameAndPassword_ReturnNull()
+        {
+            var token = service.CreateToken(new LoginModel() { Username = "wrong", Password = "password" });
+            Assert.IsNull(token);
+        }
+
+    }
+}
---
+++ CWExerciseWebApi/Common/Authentication/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using CWExerciseWebApi.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CWExerciseWebApi.Common.Authentication
+{
+    public interface IAuthenticationService
+    {
+        string CreateToken(LoginModel login);
+    }
+}
